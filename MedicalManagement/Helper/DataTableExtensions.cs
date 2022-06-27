58509eacd37105aad10eaec0fd3591713ce641fa
phucpt <phucpt@moolapp.com>
2022-06-27 23:26:37 +0700

    Add Helper Package: Add DataTableExtensions and SqlHelper file.

---
+++ MedicalManagement/Helper/DataTableExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalManagement.Helper
+{
+    public static class DataTableExtensions
+    {
+        public static T ToEntity<T>(this DataTable table) where T : new()
+        {
+            T entity = new T();
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var item in entity.GetType().GetProperties())
+                {
+                    if (row.Table.Columns.Contains(item.Name))
+                    {
+                        if (DBNull.Value != row[item.Name])
+                        {
+                            Type newType = item.PropertyType;
+
+                            if (newType.IsGenericType
+                                    && newType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                            {
+                                System.ComponentModel.NullableConverter nullableConverter = new System.ComponentModel.NullableConverter(newType);
+                                newType = nullableConverter.UnderlyingType;
+                            }
+
+                            item.SetValue(entity, Convert.ChangeType(row[item.Name], newType), null);
+
+                        }
+
+                    }
+                }
+            }
+
+            return entity;
+        }
+
+        public static List<T> ToEntities<T>(this DataTable table) where T : new()
+        {
+            List<T> entities = new List<T>();
+            if (table == null)
+                return null;
+            foreach (DataRow row in table.Rows)
+            {
+                T entity = new T();
+                foreach (var item in entity.GetType().GetProperties())
+                {
+                    if (table.Columns.Contains(item.Name))
+                    {
+                        if (DBNull.Value != row[item.Name])
+                        {
+                            Type newType = item.PropertyType;
+
+                            if (newType.IsGenericType && newType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                            {
+                                System.ComponentModel.NullableConverter nullableConverter = new System.ComponentModel.NullableConverter(newType);
+                                newType = nullableConverter.UnderlyingType;
+                            }
+                            item.SetValue(entity, Convert.ChangeType(row[item.Name], newType), null);
+                        }
+                    }
+                }
+                entities.Add(entity);
+            }
+            return entities;
+        }
+    }
+}
