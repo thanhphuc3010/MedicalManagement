11674d64782e1892e992900522d710bf0f39253e
phucpt <phucpt@moolapp.com>
2022-06-27 23:35:54 +0700

    Update DataTableExtensions.cs

--- MedicalManagement/Helper/DataTableExtensions.cs
+++ MedicalManagement/Helper/DataTableExtensions.cs
@@ -12,6 +12,7 @@ namespace MedicalManagement.Helper
         public static T ToEntity<T>(this DataTable table) where T : new()
         {
             // Phucpt changed code in this line for demo git pull
+            // Phucpc changed to make conflict
             T entity = new T();
             foreach (DataRow row in table.Rows)
             {
