f45aa2b6c6e25345af518b8b6cf53aaa0547464b
phucpt <phucpt@moolapp.com>
2022-07-02 17:00:35 +0700

    revert code

--- MedicalManagement/Program.cs
+++ MedicalManagement/Program.cs
@@ -17,7 +17,7 @@ namespace MedicalManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmCustomer());
         }
     }
 }
